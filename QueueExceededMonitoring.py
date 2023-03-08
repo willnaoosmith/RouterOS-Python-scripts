1a0647e15fafbb12335ecc4272c2038ab6b411b1
William Brochensque Júnior <55159911+willnaoosmith@users.noreply.github.com>
2023-03-08 15:40:12 -0300

    Added script to monitor queues usage
    Simple Python Script to print all current queues that are limited on download or upload and are currently exceeding that limit.

---
+++ QueueExceededMonitoring.py
@@ -0,0 +1,30 @@
+connection = routeros_api.RouterOsApiPool(
+    "host",
+    username="username",
+    password="password",
+    port=port,
+	plaintext_login=True
+)
+
+api = connection.get_api()
+
+list_queues = api.get_resource("/queue/simple")
+
+Fetchedresults = [
+	{
+		"name": queue["name"],
+		"LimitUpload": int(queue["limit-at"].split("/")[0]),
+		"LimitDownload": int(queue["limit-at"].split("/")[1]),
+		"usageUpload": int(queue["rate"].split("/")[0]),
+		"usageDownload": int(queue["rate"].split("/")[1])
+	}
+	for queue in list_queues.get()
+]
+
+ExceededQueues = [
+	queue for queue in Fetchedresults
+	if ((queue["usageUpload"] > queue["LimitUpload"]) or (queue["usageDownload"] > queue["LimitDownload"]))
+	and (queue["LimitUpload"] != 0 or queue["LimitDownload"] != 0)
+]
+
+print(ExceededQueues)
